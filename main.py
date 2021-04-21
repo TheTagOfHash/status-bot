56169207909931ddf091cbf58859b2ca0166ec81
TheTagOfHash <63460390+TheTagOfHash@users.noreply.github.com>
2021-04-21 09:29:45 -0400

    Create main.py
    basically the entire bot

---
+++ main.py
@@ -0,0 +1,59 @@
+import discord
+from discord.utils import get
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="r?", intents=discord.Intents.all())
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged into {bot.user}')
+
+
+@bot.command(aliases=['presence'])
+# @commands.has_permissions(send_messages=True)
+async def status(ctx, status_type: str, *, msg):
+    if ctx.author == bot.user:
+        return
+    await ctx.message.delete()
+    if status_type.lower() == 'p' or status_type.lower() == 'playing':
+        await bot.change_presence(activity=discord.Game(name=msg))
+        embed = discord.Embed(title='Status changed!', colour=discord.Colour.orange())
+        embed.add_field(name=f'Status is now "Playing {msg}"!', value='Enjoy the beautiful new status!', inline=False)
+        await ctx.send(embed=embed)
+    elif status_type.lower() == 'l' or status_type.lower() == 'listening':
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=msg))
+        embed = discord.Embed(title='Status changed!', colour=discord.Colour.orange())
+        embed.add_field(name=f'Status is now "Listening to {msg}"!', value='Enjoy the beautiful new status!',
+                        inline=False)
+        await ctx.send(embed=embed)
+    elif status_type.lower() == 'w' or status_type.lower() == 'watching':
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=msg))
+        embed = discord.Embed(title='Status changed!', colour=discord.Colour.orange())
+        embed.add_field(name=f'Status is now "Watching {msg}"!', value='Enjoy the beautiful new status!', inline=False)
+        await ctx.send(embed=embed)
+
+
+@bot.command()
+# @commands.has_permissions(send_messages=True)
+async def statusrole(ctx, checkstring: str, role: int):
+    if get(ctx.guild.roles, id=role):
+        roleObj = get(ctx.guild.roles, id=role)
+    else:
+        await ctx.send(f":PepeHmm: I couldn't find a role with id `{role}`")
+        return
+    await ctx.send(":PepeOk: Please wait. This might take a bit")
+    status_members = []
+    for member in ctx.guild.members:
+        for s in member.activities:
+            if isinstance(s, discord.CustomActivity) and checkstring in str(s):
+                status_members.append(member)
+    if len(status_members) == 0:
+        await ctx.send(f":sadge: I couldn't find anybody with \"{checkstring}\" in their status")
+        return
+    for member in status_members:
+        await member.add_roles(roleObj)
+    await ctx.send(f":AceHeadpat: Added role with id `{roleObj.id}` to {str(len(status_members))} member(s)")
+
+
+bot.run("id here ace")
